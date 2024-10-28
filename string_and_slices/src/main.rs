cd99ffd1d8079e0018f371c0403afa8714888153
youdianshekong <liuyang16@stu.sau.edu.cn>
2024-10-28 08:32:12 +0000

    commit all changes

--- string_and_slices/src/main.rs
+++ string_and_slices/src/main.rs
@@ -25,7 +25,7 @@ fn first_word(s: &String) -> &str {
     &s[..1]
 } */
 
-fn main() {
+/* fn main() {
     let s = String::from("hello, world!");
     say_hello(&s);
     say_hello(&s[..]);
@@ -34,4 +34,54 @@ fn main() {
 
 fn say_hello(s: &str) {
     println!("{}", s);
-}
+} */
+
+/* fn main() {
+    let mut s = String::from("Hello ");
+
+    s.push_str("rust");
+    println!("追加字符串 push_str() -> {}", s);
+
+    s.push('!');
+    println!("追加字符串 push() -> {}", s);
+} */
+
+/* fn main() {
+    let mut s = String::from("Hello rust!");
+    s.insert(5, ',');
+    println!("插入字符 insert() -> {}", s);
+    s.insert_str(6, " I like");
+    println!("插入字符串 insert_str() -> {}", s);
+} */
+
+/* fn main() {
+    let string_replace = "I like rust. Learning rust is my favorite!";
+    let new_string_replacen = string_replace.replacen("rust", "RUST", 1);
+    dbg!(new_string_replacen);
+} */
+
+/* fn main() {
+    let mut string_pop = String::from("rust pop 中文!");
+    let p1 = string_pop.pop();
+    let p2 = string_pop.pop();
+    dbg!(p1);
+    dbg!(p2);
+    dbg!(string_pop);
+} */
+
+/* fn main() {
+    let mut string_remove = String::from("测试remove方法");
+    println!(
+        "string_remove 占 {} 个字节",
+        std::mem::size_of_val(string_remove.as_str())
+    );
+    string_remove.remove(0);
+    dbg!(string_remove);
+} */
+
+/* fn main() {
+    let mut string_truncate = String::from("测试truncate");
+    string_truncate.truncate(3);
+    dbg!(string_truncate);
+} */
+
